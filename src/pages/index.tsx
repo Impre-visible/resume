ea4e7912849d8334e0283fa17d6f7b51b726cd78
Impre-visible <romeo@chevrier.dev>
2025-05-10 18:06:28 +0200

    ✨ feat: Integrate Education component to display educational information in the main layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import resume from '../assets/resume.json';
 import Basics from './_components/basics';
+import Education from './_components/education';
 
 export default function Home() {
     return (
-        <section className="flex flex-col gap-8 items-center h-screen w-screen">
-            <section className="flex flex-col gap-8 items-start justify-center h-screen w-3xl">
+        <section className="flex flex-col gap-8 items-center justify-start p-24 h-full w-screen">
+            <section className="flex flex-col gap-6 items-start justify-center w-3xl">
                 <Basics basics={resume.basics} profiles={resume.sections.profiles.items} />
+                <Education education={resume.sections.education} />
             </section>
         </section>
     );
