22170367513136c1fb02a51b7bb9176af8c90ed8
Impre-visible <romeo@chevrier.dev>
2025-05-10 17:48:07 +0200

    ✨ feat: Integrate Basics component to display personal information from resume.json

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
+import resume from '../assets/resume.json';
+import Basics from './_components/basics';
+
 export default function Home() {
     return (
-        <section className="flex flex-col gap-8 items-center justify-center h-screen bg-gray-100">
-            <h1 className="font-bold text-3xl" >Welcome to my resume.</h1>
-            <p>
-                This is a simple resume website built with React and Vite. It uses the Generouted plugin for routing.
-                <br />
-                Shadcn/ui is used for the UI components, and Tailwind CSS is used for styling.
-            </p>
+        <section className="flex flex-col gap-8 items-center h-screen w-screen">
+            <section className="flex flex-col gap-8 items-start justify-center h-screen w-3xl">
+                <Basics basics={resume.basics} profiles={resume.sections.profiles.items} />
+            </section>
         </section>
-    )
+    );
 }
