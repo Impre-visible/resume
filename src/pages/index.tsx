23e352f414c2f40083173d072cbca4a25d608be2
Impre-visible <romeo@chevrier.dev>
2025-05-10 16:33:18 +0200

    ✨ feat: Update Home page layout with Tailwind CSS for improved styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 export default function Home() {
     return (
-        <div>
-            <h1>Welcome to my resume.</h1>
+        <section className="flex flex-col gap-8 items-center justify-center h-screen bg-gray-100">
+            <h1 className="font-bold text-3xl" >Welcome to my resume.</h1>
             <p>
                 This is a simple resume website built with React and Vite. It uses the Generouted plugin for routing.
                 <br />
                 Shadcn/ui is used for the UI components, and Tailwind CSS is used for styling.
             </p>
-        </div>
+        </section>
     )
 }
