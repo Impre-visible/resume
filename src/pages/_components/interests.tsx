915a7aff5ded19ed838f7c80bf4a9aba28724701
Impre-visible <romeo@chevrier.dev>
2025-05-10 18:37:39 +0200

    ✨ feat: Add Interests component to display personal interests in the main layout

---
+++ src/pages/_components/interests.tsx
@@ -0,0 +1,36 @@
+import resume from '@/assets/resume.json';
+import { Separator } from '@/components/ui/separator';
+
+export default function Interests() {
+    return (
+        <>
+            <Separator className="w-full bg-primary" />
+            <div className="flex flex-col items-start justify-between gap-8 h-fit w-full rounded-lg">
+                <h1 className="font-bold text-3xl">{resume.sections.interests.name}</h1>
+                <div className="flex flex-col items-start flex-wrap gap-2">
+                    {resume.sections.interests.items.map((interestItem) => (
+                        interestItem.visible && (
+                            <div key={interestItem.id} className="flex flex-row items-start gap-2">
+                                <h2 className="font-mono">
+                                    {interestItem.name}
+                                    {interestItem.keywords.length > 0 && (
+                                        <span className="text-gray-500"> - </span>
+                                    )}
+                                    {interestItem.keywords.map((keyword, index) => (
+                                        <span key={index} className="text-gray-500">
+                                            {keyword}
+                                            {index < interestItem.keywords.length - 1 && (
+                                                <span className="text-gray-500">, </span>
+                                            )}
+                                        </span>
+                                    ))}
+                                </h2>
+
+                            </div>
+                        )
+                    ))}
+                </div>
+            </div>
+        </>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,22 @@
 import Basics from './_components/basics';
 import Education from './_components/education';
 import Experience from './_components/experience';
+import Interests from './_components/interests';
 import Languages from './_components/languages';
 import Projects from './_components/projects';
 import Skills from './_components/skills';
 
 export default function Home() {
     return (
-        <section className="flex flex-col gap-8 items-center justify-start p-24 h-full w-screen max-w-screen overflow-x-hidden">
-            <section className="flex flex-col gap-6 items-start justify-center w-3xl">
+        <section className="flex flex-col gap-8 items-center justify-start py-4 px-2 md:p-24 h-full w-screen max-w-screen overflow-x-hidden">
+            <section className="flex flex-col gap-6 items-start justify-center md:w-3xl">
                 <Basics />
                 <Languages />
                 <Education />
                 <Experience />
                 <Projects />
                 <Skills />
+                <Interests />
             </section>
         </section>
     );
