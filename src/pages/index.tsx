e5383b1260242c487995977b73223b079e67feab
Impre-visible <romeo@chevrier.dev>
2025-05-10 16:29:25 +0200

    ✨ feat: Add Generouted for routing and create Home page

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+    return (
+        <div>
+            <h1>Welcome to my resume.</h1>
+            <p>
+                This is a simple resume website built with React and Vite. It uses the Generouted plugin for routing.
+                <br />
+                Shadcn/ui is used for the UI components, and Tailwind CSS is used for styling.
+            </p>
+        </div>
+    )
+}
